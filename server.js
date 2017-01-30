a00821a3f65acd4420f2959f80cc1e7b3dc1c69c
Phillip Eade <Phillip Eade>
2017-01-30 17:29:20 +1100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var express = require('express');
 //Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-app.use(function(req,res, next ){
-  if(req.header['x-forwarded-proto'] === 'http'){
-    next();
-  } else {
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 app.use(express.static('public'));
