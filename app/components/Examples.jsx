09cd5442ae66a8fb89568f012f27bd8e38910b36
Phillip Eade <Phillip Eade>
2017-01-31 16:04:30 +1100

    Adding some Foundation centering

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-
+var {Link} = require("react-router");
 var Examples = (props) => {
   return(<div>
-    <h3>Examples </h3>
-    <h3>blah blah</h3>
+    <h3 className="text-ceneter">Examples </h3>
+    <p>Here are a few locations to try out</p>
+    <ol>
+      <li>
+          <Link to='/?location=Sydney'>Sydney, Australia</Link>
+      </li>
+      <li>
+        <Link to='/?location=NewYork'>New York</Link>
+      </li>
+      <li>
+            <Link to='/?location=Tokyo'>Tokyo</Link>
+      </li>
+    </ol>
   </div>
   );
 };
