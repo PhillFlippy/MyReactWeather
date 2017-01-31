8abf4e620009a2be94a4c075e88aa13617d7633e
Phillip Eade <Phillip Eade>
2017-01-31 16:37:29 +1100

    Add a cool x button to About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-// var About = React.createClass({
-//   render: function(){
-//     return(
-//       <h3>About component </h3>
-//     );
-//   }
-// });
 var About = (props) => {
-  return(<div>
+  return(<div className="text-ceneter">
     <h3>About</h3>
-    <p>This is an example of an api being used in ReactJS.</p>
-  </div>
+    <div className="alert callout" data-closable=''>
+    <p>
+      This is an example of an api being used in ReactJS. I am using this to showcase my ReactJS skills.
+    </p>
+    <button className="close-button" aria-label="Dismiss alert" type="button" data-close="">
+   <span aria-hidden="true">&times;</span>
+ </button>
+  </div></div>
   );
 };
 module.exports = About;
